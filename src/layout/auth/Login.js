efb25a54d0be5f385b272442f9ab76d4927ed0b8
KN-1111 <106130549+KN2222@users.noreply.github.com>
2023-08-28 22:18:50 +0700

    add navbar

--- src/layout/auth/Login.js
+++ src/layout/auth/Login.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
 import { useAuth } from '../../store/authContext';
-
+import { HomeNav } from '../HomeNav';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -57,93 +57,113 @@ const Login = () => {
   };
 
   return (
-    <div className='container mt-5'>
-      {/* Login Form */}
-      <form onSubmit={handleSubmit}>
-        <div className='mb-3'>
-          <label
-            htmlFor='email'
-            className='form-label'
+    <div>
+      <HomeNav />
+      <div className='container mt-5'>
+        {/* Login Form */}
+        <form onSubmit={handleSubmit}>
+          <div className='mb-3'>
+            <label
+              htmlFor='email'
+              className='form-label'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className='mb-3'>
+            <label
+              htmlFor='password'
+              className='form-label'
+            >
+              Password
+            </label>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button
+            type='submit'
+            className='btn btn-primary'
           >
-            Email
-          </label>
-          <input
-            type='email'
-            className='form-control'
-            id='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className='mb-3'>
-          <label
-            htmlFor='password'
-            className='form-label'
-          >
-            Password
-          </label>
-          <input
-            type='password'
-            className='form-control'
-            id='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button
-          type='submit'
-          className='btn btn-primary'
-        >
-          Login
-        </button>
-      </form>
+            Login
+          </button>
+        </form>
 
-      {/* Success/Error Modal */}
-      <div
-        className={`modal fade ${showModal ? 'show' : ''}`}
-        style={{ display: showModal ? 'block' : 'none' }}
-        tabIndex='-1'
-        role='dialog'
-      >
+        {/* Success/Error Modal */}
         <div
-          className='modal-dialog'
-          role='document'
+          className={`modal fade ${showModal ? 'show' : ''}`}
+          style={{ display: showModal ? 'block' : 'none' }}
+          tabIndex='-1'
+          role='dialog'
         >
-          <div className='modal-content'>
-            <div className='modal-header'>
-              <h5 className='modal-title'>
-                {successMessage ? 'Success' : 'Error'}
-              </h5>
-              <button
-                type='button'
-                className='close'
-                data-dismiss='modal'
-                aria-label='Close'
-                onClick={closeModal}
-              >
-                <span aria-hidden='true'>&times;</span>
-              </button>
-            </div>
-            <div className='modal-body'>{successMessage || errorMessage}</div>
-            <div className='modal-footer'>
-              <button
-                type='button'
-                className='btn btn-secondary'
-                data-dismiss='modal'
-                onClick={closeModal}
-              >
-                Close
-              </button>
+          <div
+            className='modal-dialog'
+            role='document'
+          >
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title'>
+                  {successMessage ? 'Success' : 'Error'}
+                </h5>
+                <button
+                  type='button'
+                  className='close'
+                  data-dismiss='modal'
+                  aria-label='Close'
+                  onClick={closeModal}
+                >
+                  <span aria-hidden='true'>&times;</span>
+                </button>
+              </div>
+              <div className='modal-body'>{successMessage || errorMessage}</div>
+              {successMessage ? (
+                <>
+                  <div className='modal-footer'>
+                    <button
+                      type='button'
+                      className='btn btn-secondary'
+                      data-dismiss='modal'
+                      onClick={() => navigate('/')}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className='modal-footer'>
+                    <button
+                      type='button'
+                      className='btn btn-secondary'
+                      data-dismiss='modal'
+                      onClick={closeModal}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </div>
+        <div
+          className={`modal-backdrop fade ${showModal ? 'show' : ''}`}
+          style={{ display: showModal ? 'block' : 'none' }}
+        ></div>
       </div>
-      <div
-        className={`modal-backdrop fade ${showModal ? 'show' : ''}`}
-        style={{ display: showModal ? 'block' : 'none' }}
-      ></div>
     </div>
   );
 };
--- src/layout/auth/SignUp.js
+++ src/layout/auth/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { HomeNav } from '../HomeNav';
 
 const SignUp = () => {
   const [name, setName] = useState('');
@@ -59,141 +60,146 @@ const SignUp = () => {
   };
 
   return (
-    <div className='container mt-5'>
-      <form onSubmit={handleSubmit}>
-        <div className='mb-3'>
-          <label
-            htmlFor='name'
-            className='form-label'
-          >
-            Name
-          </label>
-          <input
-            type='text'
-            className='form-control'
-            id='name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className='mb-3'>
-          <label
-            htmlFor='email'
-            className='form-label'
-          >
-            Email
-          </label>
-          <input
-            type='email'
-            className='form-control'
-            id='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className='mb-3'>
-          <label
-            htmlFor='password'
-            className='form-label'
-          >
-            Password
-          </label>
-          <input
-            type='password'
-            className='form-control'
-            id='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className='mb-3'>
-          <label className='form-label'>Role</label>
-          <div className='form-check'>
-            <input
-              className='form-check-input'
-              type='radio'
-              name='role'
-              id='customerRole'
-              value='customer'
-              checked={role === 'customer'}
-              onChange={() => setRole('customer')}
-            />
+    <div>
+      <HomeNav />
+      <div className='container mt-5'>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-3'>
             <label
-              className='form-check-label'
-              htmlFor='userRole'
+              htmlFor='name'
+              className='form-label'
             >
-              Customer
+              Name
             </label>
-          </div>
-          <div className='form-check'>
             <input
-              className='form-check-input'
-              type='radio'
-              name='role'
-              id='sellerRole'
-              value='seller'
-              checked={role === 'seller'}
-              onChange={() => setRole('seller')}
+              type='text'
+              className='form-control'
+              id='name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
             />
-            <label
-              className='form-check-label'
-              htmlFor='sellerRole'
-            >
-              Seller
-            </label>
           </div>
-        </div>
-        <button
-          type='submit'
-          className='btn btn-primary'
-        >
-          Register
-        </button>
-      </form>
-      <div
-        className={`modal fade ${showModal ? 'show' : ''}`}
-        style={{ display: showModal ? 'block' : 'none' }}
-        tabIndex='-1'
-        role='dialog'
-      >
+          <div className='mb-3'>
+            <label
+              htmlFor='email'
+              className='form-label'
+            >
+              Email
+            </label>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className='mb-3'>
+            <label
+              htmlFor='password'
+              className='form-label'
+            >
+              Password
+            </label>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className='mb-3'>
+            <label className='form-label'>Role</label>
+            <div className='form-check'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='role'
+                id='customerRole'
+                value='customer'
+                checked={role === 'customer'}
+                onChange={() => setRole('customer')}
+              />
+              <label
+                className='form-check-label'
+                htmlFor='userRole'
+              >
+                Customer
+              </label>
+            </div>
+            <div className='form-check'>
+              <input
+                className='form-check-input'
+                type='radio'
+                name='role'
+                id='sellerRole'
+                value='seller'
+                checked={role === 'seller'}
+                onChange={() => setRole('seller')}
+              />
+              <label
+                className='form-check-label'
+                htmlFor='sellerRole'
+              >
+                Seller
+              </label>
+            </div>
+          </div>
+          <button
+            type='submit'
+            className='btn btn-primary'
+          >
+            Register
+          </button>
+        </form>
         <div
-          className='modal-dialog'
-          role='document'
+          className={`modal fade ${showModal ? 'show' : ''}`}
+          style={{ display: showModal ? 'block' : 'none' }}
+          tabIndex='-1'
+          role='dialog'
         >
-          <div className='modal-content'>
-            <div className='modal-header'>
-              <h5 className='modal-title'>{isSuccess ? 'Success' : 'Error'}</h5>
-              <button
-                type='button'
-                className='close'
-                data-dismiss='modal'
-                aria-label='Close'
-                onClick={closeModal}
-              >
-                <span aria-hidden='true'>&times;</span>
-              </button>
+          <div
+            className='modal-dialog'
+            role='document'
+          >
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title'>
+                  {isSuccess ? 'Success' : 'Error'}
+                </h5>
+                <button
+                  type='button'
+                  className='close'
+                  data-dismiss='modal'
+                  aria-label='Close'
+                  onClick={closeModal}
+                >
+                  <span aria-hidden='true'>&times;</span>
+                </button>
+              </div>
+              <div className='modal-body'>{modalMessage}</div>
+              <div className='modal-footer'>
+                <button
+                  type='button'
+                  className='btn btn-secondary'
+                  data-dismiss='modal'
+                  onClick={closeModal}
+                >
+                  Close
+                </button>
+              </div>
             </div>
-            <div className='modal-body'>{modalMessage}</div>
-            <div className='modal-footer'>
-              <button
-                type='button'
-                className='btn btn-secondary'
-                data-dismiss='modal'
-                onClick={closeModal}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>{' '}
+          </div>{' '}
+        </div>
+        <div
+          className={`modal-backdrop fade ${showModal ? 'show' : ''}`}
+          style={{ display: showModal ? 'block' : 'none' }}
+        ></div>
       </div>
-      <div
-        className={`modal-backdrop fade ${showModal ? 'show' : ''}`}
-        style={{ display: showModal ? 'block' : 'none' }}
-      ></div>
     </div>
   );
 };
